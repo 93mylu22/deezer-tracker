90960f4d31a3d3fbfc5ae634bd9596e29cdda96f
93mylu22 <93mylu22@gmail.com>
2026-09-12 01:17:58 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Deezer Tracker",
-  description: "Seguimiento diario de posiciones en charts de Deezer",
+  title: "ARMY 🇨🇴 - Music Tracker",
+  description: "Seguimiento diario de posiciones en charts colombianos",
 };
 
 export default function RootLayout({
