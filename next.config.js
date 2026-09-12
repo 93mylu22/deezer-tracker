7c3005813af9b425c0f05a387b164eb40654f97b
93mylu22 <93mylu22@gmail.com>
2026-09-12 00:58:06 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // Si publicas en https://tu-usuario.github.io/deezer-tracker (repo project page,
-  // NO un repo tipo tu-usuario.github.io), descomenta y ajusta estas dos líneas:
-  // basePath: "/deezer-tracker",
-  // assetPrefix: "/deezer-tracker/",
+  // Repo project page: https://93mylu22.github.io/deezer-tracker
+  basePath: "/deezer-tracker",
+  assetPrefix: "/deezer-tracker/",
 };
 
 module.exports = nextConfig;
